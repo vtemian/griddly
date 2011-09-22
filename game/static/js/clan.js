b81e6d41510656a9321d831295dd3e406973389d
unknown <zmeu@.(none)>
2011-09-22 19:56:49 +0300

    ranks page, alliance page, alliance page admin options + fixes.

--- game/static/js/clan.js
+++ game/static/js/clan.js
@@ -2,9 +2,11 @@ $(document).ready(function(){
     $('#make-clan-form').submit(function(){
         var username = $('#my_username').html()
         var name = $('#clan-name').val()
+        console.log('asd')
         validate($(this), function(){
             $.each(users, function(index, user){
                if(user != username){
+                   console.log('if state')
                     $.post('/alliance/request/', {'user':user, 'name':name}, function(data){
                         var obj = jQuery.parseJSON(data);
                         socket.emit('notification', obj);

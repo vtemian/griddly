1f12c8dc1bb26a7b7556874ff76294ffc58da440
vtemian <vladtemian@gmail.com>
2011-10-31 11:39:13 +0200

    comment remove

--- settings.py
+++ settings.py
@@ -4,7 +4,6 @@ DEBUG = False
 TEMPLATE_DEBUG = True
 
 ADMINS = (
-    # ('Your Name', 'your_email@example.com'),
 )
 
 MANAGERS = ADMINS
@@ -20,74 +19,40 @@ DATABASES = {
     }
 }
 
-# Local time zone for this installation. Choices can be found here:
-# http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
-# although not all choices may be available on all operating systems.
-# On Unix systems, a value of None will cause Django to use the same
-# timezone as the operating system.
-# If running in a Windows environment this must be set to the same as your
-# system time zone.
 TIME_ZONE = 'America/Chicago'
 
-# Language code for this installation. All choices can be found here:
-# http://www.i18nguy.com/unicode/language-identifiers.html
 LANGUAGE_CODE = 'en-us'
 
 SITE_ID = 1
 
-# If you set this to False, Django will make some optimizations so as not
-# to load the internationalization machinery.
 USE_I18N = True
 
-# If you set this to False, Django will not format dates, numbers and
-# calendars according to the current locale
 USE_L10N = True
 
 PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
-# Absolute filesystem path to the directory that will hold user-uploaded files.
-# Example: "/home/media/media.lawrence.com/media/"
 
 MEDIA_ROOT = os.path.abspath(os.path.join(PROJECT_ROOT, 'site_media', 'media'))
 MEDIA_URL = '/site_media/media/'
 
-# Absolute path to the directory static files should be collected to.
-# Don't put anything in this directory yourself; store your static files
-# in apps' "static/" subdirectories and in STATICFILES_DIRS.
-# Example: "/home/media/media.lawrence.com/static/"
 STATIC_ROOT = '/home/static'
 
-# URL prefix for static files.
-# Example: "http://media.lawrence.com/static/"
 STATIC_URL = '/static/'
 
-# URL prefix for admin static files -- CSS, JavaScript and images.
-# Make sure to use a trailing slash.
-# Examples: "http://foo.com/static/admin/", "/static/admin/".
 ADMIN_MEDIA_PREFIX = '/static/admin/'
 
-# Additional locations of static files
 STATICFILES_DIRS = (
-    # Put strings here, like "/home/html/static" or "C:/www/django/static".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
 )
 
-# List of finder classes that know how to find static files in
-# various locations.
 STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.FileSystemFinder',
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
-#    'django.contrib.staticfiles.finders.DefaultStorageFinder',
 )
 
-# Make this unique, and don't share it with anybody.
 SECRET_KEY = 'wholn(f3b+y7t)r=ea407zq8c80lxf-cr++lrb)aub%ml33wi$'
 
-# List of callables that know how to import templates from various sources.
 TEMPLATE_LOADERS = (
     'django.template.loaders.filesystem.Loader',
     'django.template.loaders.app_directories.Loader',
-#     'django.template.loaders.eggs.Loader',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -146,14 +111,6 @@ INSTALLED_APPS = (
     'facebook',
 )
 
-# A sample logging configuration. The only tangible logging
-# performed by this configuration is to send an email to
-# the site admins on every HTTP 500 error.
-# See http://docs.djangoproject.com/en/dev/topics/logging for
-# more details on how to customize your logging configuration.
-
-SOCKETIO_HOST = '10.0.0.60'
-SOCKETIO_PORT = '9000'
 SENTRY_CLIENT = 'raven.contrib.django.DjangoClient'
 LOGGING = {
     'version': 1,
@@ -191,11 +148,9 @@ LOGGING = {
 import logging
 from raven.contrib.django.logging import SentryHandler
 logger = logging.getLogger()
-# ensure we havent already registered the handler
 if SentryHandler not in map(type, logger.handlers):
     logger.addHandler(SentryHandler())
 
-    # Add StreamHandler to sentry's default so you can catch missed exceptions
     logger = logging.getLogger('sentry.errors')
     logger.propagate = False
     logger.addHandler(logging.StreamHandler())

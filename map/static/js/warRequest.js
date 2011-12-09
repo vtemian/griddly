44e1dbfb93871322383ea6b62c16a3fdf028ce5b
vtemian <vladtemian@gmail.com>
2011-12-09 15:04:43 +0200

    asdasd

--- map/static/js/warRequest.js
+++ map/static/js/warRequest.js
@@ -2,6 +2,7 @@ $(document).ready(function(){
     $('#warRequest').live('click', function(){
         var username = $(this).data('username');
         var id = $(this).data('id');
+        console.log('a');
         $.post('/battle/request/war', {'username': username, 'id': id}, function(data){
             var obj = jQuery.parseJSON(data);
             if(obj.nice != undefined){

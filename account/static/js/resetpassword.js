932f09250baeb7f557873ccafcf8afe572622639
vtemian <vladtemian@gmail.com>
2011-12-09 19:16:36 +0200

    asd

--- account/static/js/resetpassword.js
+++ account/static/js/resetpassword.js
@@ -3,6 +3,11 @@ $(document).ready(function(){
        var $form = $(this);
        validate($form, function(data){
            console.log(data);
+           if(data.message != undefined){
+               setTimeout(function(){
+                window.location = "/";
+               }, 400);
+           }
        });
        return false;
    });

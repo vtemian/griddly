67839a035716c926a9d2e6557f9a847cdbe1c0b2
vtemian <vladtemian@gmail.com>
2011-12-09 15:18:38 +0200

    modal close

--- map/static/js/warRequest.js
+++ map/static/js/warRequest.js
@@ -4,12 +4,13 @@ $(document).ready(function(){
         var id = $(this).data('id');
         $.post('/battle/request/war', {'username': username, 'id': id}, function(data){
             var obj = jQuery.parseJSON(data);
+            $.modal.close();
             if(obj.nice != undefined){
                 $('#notifications_bar').html("You are in war with "+username).slideDown(200).delay(1000).slideUp(200);
             }else{
                 $('#notifications_bar').html(obj.error).slideDown(200).delay(1000).slideUp(200);
             }
-            $.modal.close();
+
         })
     });
 });

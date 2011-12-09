3ca4c37e77436e7afe1cbc5abd1ae5d5d3e29b02
vtemian <vladtemian@gmail.com>
2011-12-09 19:14:54 +0200

    as

--- account/static/js/resetpassword.js
+++ account/static/js/resetpassword.js
@@ -1,7 +1,9 @@
 $(document).ready(function(){
    $('#password-form').submit(function(){
        var $form = $(this);
-       validate($form);
+       validate($form, function(data){
+           console.log(data);
+       });
        return false;
    });
 });
